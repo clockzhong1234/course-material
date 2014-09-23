4dd230b00f4799b2ce5f1756de7fb39a70b85613
Hang <zhonghang9@gmail.com>
2014-09-23 19:44:19 +0800

    Solut 100

---
+++ exercices/100/solution.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+station = {
+ 'address': 'RUE DES CHAMPEAUX (PRES DE LA GARE ROUTIERE) - 93170 BAGNOLET',
+ 'number': 31705,
+ 'latitude': 48.8645278209514,
+ 'name': 'CHAMPEAUX (BAGNOLET)',
+ 'longitude': 2.416170724425901
+}
+print("latitude"),
+print(station['latitude'])
+print("longitude"),
+print(station['longitude'])
+print("number"),
+print(station['number'])
+print("name"),
+print(station['name'])
+print("address"),
+print(station['address'])
