6e7043d1565cb6aaea5fe75b96553c72c9aedd2a
Hang <zhonghang9@gmail.com>
2014-09-23 17:40:35 +0800

    solut 050

---
+++ exercices/050/solution.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 23 00:27:56 2014
+
+@author: hang
+"""
+s=0
+for i in range(3,1000,3):
+    s = s + i
+for i in range(5,1000,5):
+    s = s + i    
+print(s)
