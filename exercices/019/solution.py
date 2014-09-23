ba046febcc6783ce45b7ca51af731555daa29111
Hang <zhonghang9@gmail.com>
2014-09-23 17:16:14 +0800

    solut 019

---
+++ exercices/019/solution.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 23 00:27:56 2014
+
+@author: hang
+"""
+
+import sys
+if sys.argv[1] == None or (sys.argv[2]) == None:
+    print("Please enter two integers.")
+else:
+    s = int(sys.argv[1]) + int(sys.argv[2])
+    print(s)
