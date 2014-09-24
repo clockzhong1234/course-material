c0eaf07b09fd8858e15f9f830b48fca5f9ada1d5
Hang <zhonghang9@gmail.com>
2014-09-24 16:23:20 +0800

    Solut 110

--- exercices/110/solution.py
+++ exercices/110/solution.py
@@ -8,27 +8,25 @@ This is a temporary script file.
 import sys
 if len(sys.argv) < 2:
     print("Please enter two integers and operator for calculation.")
-    sys.exit(1)
-a = int(sys.argv[1])
-b = int(sys.argv[3])
-c = str(sys.argv[2])
-print(c)
-
-if c == "+":
-    r = a + b
-    print(a+b)
-if c == "-":
-    r = a - b
-    print(r)
-if c == "*":
-    r = a * b
-    print(r)
-if c == "/":
-    r = a / b
-    print(r)
-if c == "%":
-    r = a % b
-    print(r)
-if c == "^":
-    r = a ** b
-    print(r)
+else:
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    c = str(sys.argv[2])
+    if c == "+":
+        r = a + b
+        print(a+b)
+    if c == "-":
+        r = a - b
+        print(r)
+    if c == "*":
+        r = a * b
+        print(r)
+    if c == "/":
+        r = a / b
+        print(r)
+    if c == "%":
+        r = a % b
+        print(r)
+    if c == "^":
+        r = a ** b
+        print(r)
