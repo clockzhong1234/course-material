d13714d6520d749dffc9495f210f18eed44adc6c
Hang <zhonghang9@gmail.com>
2014-09-24 15:13:13 +0800

    Solut 019

--- exercices/019/solution.py
+++ exercices/019/solution.py
@@ -6,8 +6,8 @@ Created on Tue Sep 23 00:27:56 2014
 """
 
 import sys
-if sys.argv[1] == None or (sys.argv[2]) == None:
-    print("Please enter two integers.")
-else:
-    s = int(sys.argv[1]) + int(sys.argv[2])
-    print(s)
+if len(sys.argv) < 2:
+    print("Please enter a parameter")
+    sys.exit(1)   
+s = int(sys.argv[1]) + int(sys.argv[2])
+print(s)
