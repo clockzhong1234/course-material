6ca5b42f8478a926f43a81d71d62487e06a95f92
Hang <zhonghang9@gmail.com>
2014-09-24 19:18:50 +0800

    Solut 210

--- exercices/210/is_prime.py
+++ exercices/210/is_prime.py
@@ -5,14 +5,16 @@ Created on Wed Sep 24 16:26:02 2014
 @author: hang
 """
 
+
 def is_prime(num):
     
     
     j = 1
     n = int(num ** 0.5) + 1
     for i in range(2, n, 1):
-        if num % i != 0:
+        if num % i == 0:
             j = 0
+            exit()
     if j == 0:
         return False
     else:
