12ea38f533e7b78a078336212002549c62615473
Hang <zhonghang9@gmail.com>
2014-09-24 16:17:24 +0800

    Solut 019

--- exercices/019/solution.py
+++ exercices/019/solution.py
@@ -3,11 +3,13 @@
 Created on Tue Sep 23 00:27:56 2014
 
 @author: hang
+sys.exit(1) 
 """
 
 import sys
 if len(sys.argv) < 2:
     print("Please enter a parameter")
-    sys.exit(1)   
-s = int(sys.argv[1]) + int(sys.argv[2])
-print(s)
+else:
+    s = int(sys.argv[1]) + int(sys.argv[2])
+    print(s)
+    
