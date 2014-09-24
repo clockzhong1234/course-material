995bcc03b4745abe59e629edd3c44cef106d39e5
Hang <zhonghang9@gmail.com>
2014-09-24 15:50:56 +0800

    Solut 110

--- exercices/110/solution.py
+++ exercices/110/solution.py
@@ -6,9 +6,13 @@ This is a temporary script file.
 """
 
 import sys
+if len(sys.argv) < 2:
+    print("Please enter two integers and operator for calculation.")
+    sys.exit(1)
 a = int(sys.argv[1])
 b = int(sys.argv[3])
 c = str(sys.argv[2])
+print(c)
 
 if c == "+":
     r = a + b
