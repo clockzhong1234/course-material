145399e7fdf4ba2ce513736bc601ae33a2d3de8a
Hang <zhonghang9@gmail.com>
2014-09-26 00:13:20 +0800

    Solut 320

--- exercices/320/solution.py
+++ exercices/320/solution.py
@@ -4,6 +4,8 @@ Created on Thu Sep 25 18:37:44 2014
 
 @author: hang
 """
+import string
+dict_al = dict.fromkeys(string.ascii_lowercase, 0)
 f = open('words', 'r')
 content = f.read()
 al = ""
@@ -12,10 +14,13 @@ for i in range(len(content)):
         al = al + content[i]
 l = len(al)
 al = al.lower()
-dict_al = { "a": 0, "b": 0, "c": 0, "d": 0, "e": 0, "f": 0, "g": 0, "h": 0, 
-"i": 0, "j": 0, "k": 0, "l": 0, "m": 0, "n": 0, "o": 0, "p": 0, "q": 0, "r": 0,
- "s": 0, "t": 0, "u": 0, "v": 0, "w": 0, "x": 0, "y": 0, "z": 0}
+#dict_al = { "a": 0, "b": 0, "c": 0, "d": 0, "e": 0, "f": 0, "g": 0, "h": 0, 
+#"i": 0, "j": 0, "k": 0, "l": 0, "m": 0, "n": 0, "o": 0, "p": 0, "q": 0, "r": 0,
+ #"s": 0, "t": 0, "u": 0, "v": 0, "w": 0, "x": 0, "y": 0, "z": 0}
 for letter in al:
-    dict_al[letter] += 1/l
+    if letter in dict_al.keys():
+        dict_al[letter] += 1/l
 for key in dict_al:
     print(key, ": ", '%.2f' % dict_al[key])
+    
+
