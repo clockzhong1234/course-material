0d73bc8c6dceedfe45e572e21e689b5d8ddcdeca
Hang <zhonghang9@gmail.com>
2014-09-26 01:30:46 +0800

    Solut 320

--- exercices/320/solution.py
+++ exercices/320/solution.py
@@ -12,15 +12,13 @@ al = ""
 for i in range(len(content)):
     if content[i].isalpha() == True:
         al = al + content[i]
-l = len(al)
+count = 0
 al = al.lower()
-#dict_al = { "a": 0, "b": 0, "c": 0, "d": 0, "e": 0, "f": 0, "g": 0, "h": 0, 
-#"i": 0, "j": 0, "k": 0, "l": 0, "m": 0, "n": 0, "o": 0, "p": 0, "q": 0, "r": 0,
- #"s": 0, "t": 0, "u": 0, "v": 0, "w": 0, "x": 0, "y": 0, "z": 0}
 for letter in al:
     if letter in dict_al.keys():
-        dict_al[letter] += 1/l
+        dict_al[letter] += 1
+        count += 1
 for key in dict_al:
-    print(key, ": ", '%.2f' % dict_al[key])
+    print("{}: {:.2f}".format(key, dict_al[key] / count))
     
 
