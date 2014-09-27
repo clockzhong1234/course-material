254315ad67c20a978bb47a9ee6d32bd28daf550f
Iryna Nikolayeva <iryna.nikolayeva@sprint-je.com>
2014-09-27 17:18:01 +0200

    founds number of articles

--- youplaboum/747/solution.py
+++ youplaboum/747/solution.py
@@ -7,9 +7,20 @@ Created on Sat Sep 27 11:17:39 2014
 
 import requests
 from bs4 import BeautifulSoup as bsoup
-r = requests.get('http://www.openwetware.org/wiki/Labs')
+r = requests.get('http://www.ncbi.nlm.nih.gov/pubmed/?term=gene+network')
 doc = r.text
+with open('fpubmed.html', "w") as fpubmed:
+    fpubmed.write(doc)
 soup = bsoup(doc)
-print(soup.title)
-print(soup.title.text)
-print(soup.toggler)
+#print(soup.title)
+#print(doc)
+"""print(soup.title.text)"""
+
+count = soup.find_all('meta', {'name':'ncbi_resultcount'})[0]
+print(count.attrs)
+print(count['content'])
+
+
+
+
+
